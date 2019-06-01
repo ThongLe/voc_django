231c027a90f6b8f3f3ffbceafb47e482f0870daf
ThongLe <lpthong90@gmail.com>
2019-06-01 13:37:47 +0800

    initialize

---
+++ vocdjango/settings/prod.py
@@ -0,0 +1,17 @@
+from vocdjango.settings import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT'),
+    }
+}
