e28cd9bc1ae2986908b5ca617ec1571202bd6f4c
ThongLe <lpthong90@gmail.com>
2019-06-02 11:25:18 +0800

    fix tasks table

--- api/asana/serializers.py
+++ api/asana/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from rest_framework.relations import HyperlinkedIdentityField
 
 from asana.models import Task
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskUrlHyperlinkedRelatedField(HyperlinkedIdentityField):
+    def __init__(self, view_name=None, **kwargs):
+        assert view_name is not None, 'The `view_name` argument is required.'
+        kwargs['read_only'] = True
+        kwargs['source'] = '*'
+        super(TaskUrlHyperlinkedRelatedField, self).__init__('asana:tasks:view', lookup_url_kwarg='task_id')
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    serializer_url_field = TaskUrlHyperlinkedRelatedField
+
     class Meta:
         model = Task
-        fields = ('name', 'start_time', 'required_hours')
+        fields = ('name', 'start_time', 'required_hours', 'url')
--- api/asana/views.py
+++ api/asana/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from asana.models import Task
 
@@ -8,3 +9,8 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'data': serializer.data})
